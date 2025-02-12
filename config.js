72bd391e875ab94eb230bc800a6bf55a6e1167bc
GABZINHOOO <navaljangabrielle@gmail.com>
2025-02-12 23:30:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ animations: {
 music: {
     enabled: true, // Music feature is enabled
     autoplay: true, // Try to autoplay (note: some browsers may block this)
-    musicUrl: "https://asset.cloudinary.com/dzkgafddg/5c7fce91ead1ffe4b67423614358162b", // Paste your music URL here
+    musicUrl: "YOUR_CLOUDINARY_URL_HERE", // Paste your music URL here
     startText: "🎵 Play Music", // Button text to start music
     stopText: "🔇 Stop Music", // Button text to stop music
     volume: 0.5 // Volume level (0.0 to 1.0)
